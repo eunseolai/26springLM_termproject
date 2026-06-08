be0a5b05c4f7ce41a03f8c320a0773477f5857d0
Eunseo Ko <eunseo01717@hufs.ac.kr>
2026-06-08 21:11:20 +0900

    deepcopy2

--- src/peft/prefix_tuning.py
+++ src/peft/prefix_tuning.py
@@ -1,10 +1,10 @@
+from copy import deepcopy
 from peft import PrefixTuningConfig, get_peft_model
 from src.config import PT_DEFAULTS
-from copy import deepcopy
 
 def apply_prefix_tuning(model, num_virtual_tokens=None):
 
-    pt_config = PT_DEFAULTS.deepcopy()
+    pt_config = deepcopy(PT_DEFAULTS)
 
     if num_virtual_tokens is not None:
         pt_config["num_virtual_tokens"] = num_virtual_tokens
